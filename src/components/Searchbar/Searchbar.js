750c847eab1df3ad1c120a8921549d44946372ad
Petro Babiuk <75635325+PetroBabiuk@users.noreply.github.com>
2021-09-12 16:37:48 +0300

    Update Searchbar.js

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -32,7 +32,7 @@ const Searchbar = ({onSubmit}) => {
                     type="text"
                     autoComplete="off"
                     autoFocus
-                    placeholder="Search images and photos"
+                    placeholder="Search movies"
                     value={inputValue}
                     onChange={handleInputChange}
                 />
@@ -44,49 +44,5 @@ const Searchbar = ({onSubmit}) => {
 Searchbar.propTypes = {
     onSubmit: PropTypes.func.isRequired,
 }
-
-// class OldSearchbar extends Component {
-//     state = {
-//         inputValue: '',
-//     };
-
-    // handleInputChange = e => {
-    //     this.setState({ inputValue: e.currentTarget.value });
-    // };
-
-    // handleSubmit = e => {
-    //     e.preventDefault();
-
-    //     if (this.state.inputValue.trim() === '') {
-    //       return toast.error('Please do not use empty string, you have to write something');
-    //     }
-
-    //     this.props.onSubmit(this.state.inputValue);
-    //     this.setState({ inputValue: '' });
-    // };
-
-    
-//     render() {
-        // return (
-        //     <header className="Searchbar">
-        //         <form className="SearchForm" onSubmit={this.handleSubmit}>
-        //             <button type="submit" className="SearchForm-button">
-        //                 <span className="SearchForm-button-label">Search</span>
-        //             </button>
-
-        //             <input
-        //                 className="SearchForm-input"
-        //                 type="text"
-        //                 autoComplete="off"
-        //                 autoFocus
-        //                 placeholder="Search images and photos"
-        //                 value={this.state.inputValue}
-        //                 onChange={this.handleInputChange}
-        //                 />
-        //         </form>
-        //     </header>
-        // );
-//     };
-// };
 
 export default Searchbar;
