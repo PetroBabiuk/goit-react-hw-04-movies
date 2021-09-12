8dce4989189f5ae22c008df8dbf007437f9e8faa
Petro Babiuk <75635325+PetroBabiuk@users.noreply.github.com>
2021-09-12 16:38:03 +0300

    Update Cast.js

--- src/views/Cast.js
+++ src/views/Cast.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
+// import { useParams } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { fetchMovieCast } from "servises/moviesApi";
 
 
@@ -13,12 +13,14 @@ function Cast({ movieId }) {
 
     return (
         <>
-             {actors.length === 0
+            {actors.length === 0
                 ? <p>We don't have any information about actors for this movie</p>
                 : (<ul>
                     {actors.map(actor =>
                         <li key={actor.id}>
-                            <img src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`} alt={actor.name} />
+                            {actor.profile_path
+                                ? (<img src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`} alt={actor.name} />)
+                                : (<img src="http://placehold.it/200x300/000000/FFFFFF/?text=There+is+no+photo" alt={actor.name} />)}
                             <p>{actor.name}</p>
                         </li>
                     )}
